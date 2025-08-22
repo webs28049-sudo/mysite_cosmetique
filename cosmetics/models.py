948e81453e720d2df9661fa52a5c1ed1d20c33aa
your lamia <webs28049@gmail.com>
2025-08-22 23:19:16 +1200

    Configure PostagreSQL database

--- cosmetics/models.py
+++ cosmetics/models.py
@@ -21,7 +21,7 @@ class Cosmetic(models.Model):
 class LoginCosmetic(models.Model):
     name=models.CharField(max_length=100)
     phone=models.CharField(max_length=100, null=True, blank=True)
-    address=models.CharField(null=True, blank=True)
+    address=models.CharField(max_length=100, null=True, blank=True)
     product=models.TextField(null=True, blank=True)
 
     def __str__(self):
--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 
@@ -75,11 +76,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
     }
+else:
+  DATABASES = {
+     'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / "db.sqlite3",
+            }
+
 }
 
 
